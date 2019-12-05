1659d0a7d1566ceef6c9dd504cfc13903a7c760d
sangxiang <1005015165@qq.com>
2019-12-05 09:34:22 +0800

    12月5号 归档

--- app/src/main/java/com/xinfu/qianxiaozhuang/utils/ResourcesUtils.kt
+++ app/src/main/java/com/xinfu/qianxiaozhuang/utils/ResourcesUtils.kt
@@ -1,11 +1,14 @@
 package com.xinfu.qianxiaozhuang.utils
 
+import android.support.annotation.ColorRes
+import android.support.annotation.StringRes
 import android.support.v4.content.ContextCompat
 import com.xinfu.qianxiaozhuang.App
 class ResourcesUtils{
     companion object {
-       fun getColor(id:Int):Int{
+       fun getColor(@ColorRes id:Int):Int{
          return  ContextCompat.getColor(App.getInstance(),id)
        }
+
     }
 }
